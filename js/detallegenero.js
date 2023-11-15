88f3a1547b8311e21cf2a01f4bef398e57986dd6
Ines Sere <inessere@MacBook-Air-de-Ines.local>
2023-11-15 14:00:12 -0300

    sigo detalle generos

--- js/detallegenero.js
+++ js/detallegenero.js
@@ -1,8 +1,9 @@
-let api_key          = "e62f099aa015b1afedfca7df020f6e6b";
+let api_key            = "e62f099aa015b1afedfca7df020f6e6b";
 let queryString        = location.search;
 let queryStringObj     = new URLSearchParams(queryString);
-let idgenero              = queryStringObj.get('idgenero');
-let urldetallegenero =`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=${idgenero}`
+let idgenero           = queryStringObj.get('idgenero');
+let nombre             = queryStringObj.get('name'); 
+let urldetallegenero   = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=${idgenero}`
 
 fetch(urldetallegenero)
     .then(function(response){
@@ -13,16 +14,17 @@ fetch(urldetallegenero)
         console.log(data);
         let contenido = "";
         for (let index = 0; index < 5; index++) {
-           contenido += `<article class="article">
-           <a href="../pi-grupo-8-proyecto/detallepelicula.html">
-               <img class="fotobarbie" src="https://image.tmdb.org/t/p/w500/${data.results[index].backdrop_path}" alt="fotobarbie">
+            console.log(data.results[index].poster_path)
+           contenido += `
+           <article class="article">
+           <a href="../pi-grupo-8-proyecto/detallepelicula.html?">
+               <img class="fotobarbie" src="https://image.tmdb.org/t/p/w500/${data.results[index].poster_path}" alt="fotobarbie">
            </a>
            </article>` 
             
-        }
-
-        detallegenero.HTML += contenido;
-        return data;
+        };
+        let detallegenero      = document.querySelector(".peliculas");
+        detallegenero.innerHTML += contenido;
         
             
 
